4c6cd9aebace73f6c9497f73e52d688fc7bb16ca
Scott Baker <smbaker@gmail.com>
2020-08-29 09:02:26 -0700

    12v version

---
+++ ups-12v/ups-12v.ino
@@ -0,0 +1,22 @@
+/*
+ * Fuses
+ *   CKDIV8 = 1      (make sure unchecked) turn off the clock div
+ *   BODLEVEL2 = 1   bod at 2.7V (could be as low as 2.5V; low-voltage tiny85 recommended)
+ *   BODLEVEL1 = 0
+ *   BODLEVEL0 = 1
+ * 
+ *   HIGH=DA, LOW=E2, EXT=FF
+ */
+
+#include <Arduino.h>
+#include <TinyWireS.h>
+#include <util/crc16.h>
+
+#define R1 10000
+#define R2 2200
+// Note: Make sure to use a float when specifying the voltage
+#define ON_THRESH_DESIRED 11.5
+#define POWERUP_THRESH_DESIRED 8.5
+#define OFF_THRESH_DESIRED 8.0
+
+#include "../ups-common/ups.ino"
--- ups-common/ups.ino
+++ ups-common/ups.ino
@@ -1,27 +1,10 @@
-/*
- * Fuses
- *   CKDIV8 = 1      (make sure unchecked) turn off the clock div
- *   BODLEVEL2 = 1   bod at 2.7V (could be as low as 2.5V; low-voltage tiny85 recommended)
- *   BODLEVEL1 = 0
- *   BODLEVEL0 = 1
- * 
- *   HIGH=DA, LOW=E2, EXT=FF
- */
-
-#include <Arduino.h>
-#include <TinyWireS.h>
-#include <util/crc16.h>
+#ifndef R1
+#error Do not build this directly. Build either ups-5v.ino or ups-12v.in0
+#endif
 
 #ifndef TWI_RX_BUFFER_SIZE
 #define TWI_RX_BUFFER_SIZE ( 16 )
 #endif
-
-#define R1 10000
-#define R2 2200
-// Note: Make sure to use a float when specifying the voltage
-#define ON_THRESH_DESIRED 11.5
-#define POWERUP_THRESH_DESIRED 8.5
-#define OFF_THRESH_DESIRED 8.0
 
 #define VCONV(x) (x * R2 / (R1+R2))
 #define VCONV_DIG(x) VCONV(x)*256/2.56
